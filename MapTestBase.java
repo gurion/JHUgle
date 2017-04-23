13e286e8befda60b51a676f2b090a6ca4ff09b6a
Gurion Marks <gurion@jhu.edu>
2017-04-23 19:51:29 -0400

    Map interfaces

---
+++ MapTestBase.java
@@ -0,0 +1,97 @@
+//Gurion Marks
+//gmarks2
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Testing implementations of the Map interface.
+ */
+public abstract class MapTestBase {
+    protected Map<Integer, Integer> map;
+    
+    protected abstract Map<Integer, Integer> createMap();
+    
+    @Before
+    public void setupMapTest() {
+		map = this.createMap();
+    }
+    
+    @Test
+    public void testHas() {
+		map.insert(1, 1);
+		assertEquals(true, map.has(1));		
+	}
+
+	@Test
+	public void testDoesNotHave() {
+		map.insert(1,1);
+		map.remove(1);
+		assertEquals(false, map.has(1));
+	}
+
+	@Test
+	public void testGet() {
+		map.insert(1, 1);
+		Integer i = map.get(1);
+		assertEquals((int) i, 1);
+	}
+
+	@Test
+	public void testPut() {
+		map.insert(1, 1);
+		map.put(1, 2);
+		Integer i = map.get(1);
+		assertEquals((int) i, 2);
+	}
+
+	@Test
+	public void testNewEmpty() {
+		assertEquals(map.size(), 0);
+	}
+
+	@Test
+	public void testInsert() {
+		map.insert(1, 1);
+		assertEquals(map.size(), 1);
+	}
+
+	@Test
+	public void testRemove() {
+		map.insert(1, 1);
+		assertEquals(map.size(), 1);
+		map.remove(1);
+		assertEquals(map.size(), 0);
+	}
+
+	@Test (expected=IllegalArgumentException.class)
+	public void insertException() {
+		map.insert(1, 1);
+		map.insert(1, 1);
+	}
+
+	@Test (expected=IllegalArgumentException.class)
+	public void removeException() {
+		map.remove(1);
+	}
+
+	@Test (expected=IllegalArgumentException.class)
+	public void getException() {
+		map.get(1);
+	}
+
+	@Test (expected=IllegalArgumentException.class)
+	public void putException() {
+		map.put(1, 1);
+	}
+
+
+
+
+
+}
