778ac7640033e026fa364e94cf95196615c852c9
Gurion Marks <gurion@jhu.edu>
2017-05-03 12:39:31 -0400

    rehash tests for all

--- LPHashMapTest.java
+++ LPHashMapTest.java
@@ -10,10 +10,30 @@ awalke57@jhu.edu
 Assignment 9
 */
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 /** Instantiate SparseGraph to test. */
 public class LPHashMapTest extends MapTestBase {
 	@Override
 	protected Map<Integer, Integer> createMap() {
 	    return new LPHashMap<Integer, Integer>();
 	}
+
+    @Test
+    public void checkRehash() {
+        int[] ra = new int[9000];
+        for (int i = 0; i < 9000; i++) {
+            ra[i] = i;
+        }
+        for (int i : ra) {
+            map.insert((Integer) i, (Integer) i * i);
+        }
+        int size = 0;
+        for (Integer i : map) {
+            size++;
+        }
+        assertEquals(9000, size);
+        assertEquals(map.size(), size);
+    }
 }
