5dcdd3cb6cf13b273b6cf9903adfbb96fa1893c1
Gurion Marks <gurion@jhu.edu>
2017-05-03 12:25:46 -0400

    test rehash

--- ChainHashMapTest.java
+++ ChainHashMapTest.java
@@ -1,13 +1,13 @@
-/**
-    Gurion Marks
-    gmarks2
-    gurion@jhu.edu
-    Angelica Walker
-    awalke57
-    awalke57@jhu.edu
-    600.226.02
-    05/03/17
-    Assignment 9
+/*
+Gurion Marks
+gmarks2
+gurion@jhu.edu
+Angelica Walker
+awalke57
+awalke57@jhu.edu
+600.226.02
+05/03/17
+Assignment 9
 */
 
 /** Instantiate SparseGraph to test. */
@@ -16,4 +16,21 @@ public class ChainHashMapTest extends MapTestBase {
 	protected Map<Integer, Integer> createMap() {
 	    return new ChainHashMap<Integer, Integer>();
 	}
+
+    @Test
+    public void checkRehash() {
+        int[] ra = new int[9000];
+        for (int i = 0; i < 9000; i++) {
+            ra[i] = i;
+        }
+        for (int i : ra) {
+            map.insert((Integer) i, (Integer) i * i);
+        }
+        int size = 0;
+        for (Integer i : map) {
+            size++;
+        }
+        assertEquals(9000, size);
+        assertEquals(map.size(), size);
+    }
 }
